2052a068ad5f7a19a1c00fc6bb8ea21511879857
David <manukyandavid92@gmail.com>
2020-09-13 14:53:52 +0400

    one two

--- factorial.py
+++ factorial.py
@@ -1,10 +1,7 @@
 def factorial(n):
-<<<<<<< HEAD
-=======
     one = 1
     two = 2
     three = 3
->>>>>>> fc34527a452221dfa6a93b2ebc4845b4b4ea8173
     result = 1
     for i in range(1,n + 1):
         result *= i
